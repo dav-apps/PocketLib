76ddd775554c34dbf9fe0ccd87d2f7f2f02eae9e
David <dav2070@web.de>
2019-04-11 20:33:21 +0200

    Install dav-npm and initialize it in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import * as Dav from 'dav-npm';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PocketLib';
+	title = 'PocketLib';
+
+	ngOnInit(){
+		let notificationOptions = {
+			icon: "",
+			badge: ""
+		}
+
+		Dav.Initialize(environment.production ? Dav.DavEnvironment.Production : Dav.DavEnvironment.Development,
+							environment.appId,
+							[],
+							[],
+							notificationOptions,
+							{
+								UpdateAllOfTable: (tableId: number) => {
+
+								},
+								UpdateTableObject: (tableObject: Dav.TableObject) => {
+
+								},
+								DeleteTableObject: (tableObject: Dav.TableObject) => {
+
+								},
+								SyncFinished: () => {
+
+								}
+							});
+	}
 }
