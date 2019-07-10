f61972995cd18026286b6e4cb30464b2d64571d1
David <dav2070@web.de>
2019-07-11 00:20:23 +0200

    Add settings page content with app info

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from "@angular/core";
+import { environment } from 'src/environments/environment';
 
 @Component({
    selector: "pocketlib-settings-page",
    templateUrl: "./settings-page.component.html"
 })
 export class SettingsPageComponent{
+	version: string = environment.version;
 
-	constructor(){
-		
-	}
+	constructor(){}
 }
