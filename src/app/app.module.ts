2232312d54d0df9a78dee53c503543dce5d44039
David <dav2070@web.de>
2019-07-13 01:49:26 +0200

    Install office-ui-fabric-core and add context menu on right click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgxFileHelpersModule } from 'ngx-file-helpers';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material';
--- src/app/pages/library-page/library-page.component.ts
+++ src/app/pages/library-page/library-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ElementRef } from "@angular/core";
 import { Router } from '@angular/router';
 import { ReadFile } from 'ngx-file-helpers';
 import { Book } from 'src/app/models/Book';
@@ -9,11 +9,28 @@ import { DataService } from 'src/app/services/data-service';
    templateUrl: "./library-page.component.html"
 })
 export class LibraryPageComponent{
+	@ViewChild('contextMenu', {static: true}) contextMenu: ElementRef;
+	contextMenuVisible: boolean = false;
+	contextMenuPositionX: number = 0;
+	contextMenuPositionY: number = 0;
+	selectedBook: Book;
+
 	constructor(
 		private router: Router,
 		private dataService: DataService
    ){
-      this.dataService.navbarVisible = true;
+		this.dataService.navbarVisible = true;
+		document.onclick = (event: MouseEvent) => {
+			if(!this.contextMenuVisible) return;
+
+			let target = event.target as Node;
+			let contextMenu = this.contextMenu.nativeElement as HTMLDivElement;
+
+			if(!contextMenu.contains(target)){
+				// Hide the context menu
+				this.contextMenuVisible = false;
+			}
+		}
    }
 
 	async filePick(file: ReadFile){
@@ -26,4 +43,26 @@ export class LibraryPageComponent{
 		this.dataService.currentBook = book;
 		this.router.navigate(["book"]);
    }
+
+   onContextMenu(event: MouseEvent, book: Book){
+		this.selectedBook = book;
+		
+		// Set the position of the context menu
+		this.contextMenuPositionX = event.clientX;
+		this.contextMenuPositionY = event.clientY;
+
+		if(this.contextMenuVisible){
+			this.contextMenuVisible = false;
+			setTimeout(() => {
+				this.contextMenuVisible = true;
+			}, 60);
+		}else{
+			this.contextMenuVisible = true;
+		}
+		return false;
+	}
+
+	ShowDeleteBookModal(){
+		this.contextMenuVisible = false;
+	}
 }
