7ff6f745808531125f94e55b5910146856362275
David <dav2070@web.de>
2019-08-19 16:11:43 +0200

    Update the layout dynamically and add buttons

--- src/app/pages/developer-page/developer-page.component.ts
+++ src/app/pages/developer-page/developer-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
+
+const navbarHeight: number = 64;
 
 @Component({
 	selector: "pocketlib-developer-page",
@@ -8,5 +10,22 @@ import { Component } from "@angular/core";
 	]
 })
 export class DeveloperPageComponent{
+	header1Height: number = 600;
+	header1TextMarginTop: number = 200;
 
+	constructor(){}
+
+	ngOnInit(){
+		this.setSize();
+	}
+
+   @HostListener('window:resize')
+	onResize(){
+		this.setSize();
+   }
+   
+   setSize(){
+		this.header1Height = window.innerHeight - navbarHeight;
+		this.header1TextMarginTop = this.header1Height * 0.36;
+   }
 }
