270e59f742227dc49a9b6c01450956c23f7eeaa3
David <dav2070@web.de>
2019-08-19 15:33:39 +0200

    Add background image with acrylic style for landing page

--- src/app/pages/developer-page/developer-page.component.ts
+++ src/app/pages/developer-page/developer-page.component.ts
@@ -2,8 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
 	selector: "pocketlib-developer-page",
-	templateUrl: "./developer-page.component.html"
+	templateUrl: "./developer-page.component.html",
+	styleUrls: [
+		'./developer-page.component.scss'
+	]
 })
 export class DeveloperPageComponent{
-	
+
 }
