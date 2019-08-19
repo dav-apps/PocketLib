10dc69178e2194e0c7ef8ff921bd35e30edc19f3
David <dav2070@web.de>
2019-08-20 00:27:45 +0200

    Fix the server not being able to navigate directly to specific urls
    Also fix the npm commands and add build watch

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+var path = require('path');
 var express = require('express');
 
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.use("/", express.static("PocketLib"));
+function getRoot(request, response) {
+	response.sendFile(path.resolve('./PocketLib/index.html'));
+}
+
+function getUndefined(request, response) {
+	response.sendFile(path.resolve('./PocketLib/index.html'));
+}
+
+app.use(express.static('./PocketLib'));
+
+app.get('/', getRoot);
+app.get('/*', getUndefined);
 
 io.on('connection', (socket) => {
 	console.log("Connection established!");
