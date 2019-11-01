60f00e3264ab240aeaeba11ae0fe660c8f08ca2f
David <dav2070@web.de>
2019-11-01 23:56:19 +0100

    Tidy up AppModule and add WebsocketService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+
+// Modules
 import { AngularReactBrowserModule } from '@angular-react/core';
+import { AppRoutingModule } from './app-routing.module';
 import { NgxFileHelpersModule } from 'ngx-file-helpers';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material';
@@ -22,10 +26,22 @@ import {
    FabSliderModule,
    FabToggleModule
 } from '@angular-react/fabric';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+// Services
 import { DataService } from './services/data-service';
-import { AppRoutingModule } from './app-routing.module';
+import { WebsocketService } from './services/websocket-service';
+
+// Components
 import { AppComponent } from './app.component';
+import { EpubContentComponent } from './components/epub-content/epub-content.component';
+import { PdfContentComponent } from './components/pdf-content/pdf-content.component';
+import { ChaptersTreeComponent } from './components/chapters-tree/chapters-tree.component';
+import { RenameBookModalComponent } from './components/rename-book-modal/rename-book-modal.component';
+import { DeleteBookModalComponent } from './components/delete-book-modal/delete-book-modal.component';
+import { LogoutModalComponent } from './components/logout-modal/logout-modal.component';
+
+// Pages
 import { LibraryPageComponent } from './pages/library-page/library-page.component';
 import { BookPageComponent } from './pages/book-page/book-page.component';
 import { AccountPageComponent } from './pages/account-page/account-page.component';
@@ -36,18 +52,18 @@ import { NewAppPageComponent } from './pages/new-app-page/new-app-page.component
 import { AuthorPageComponent } from './pages/author-page/author-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
-import { EpubContentComponent } from './components/epub-content/epub-content.component';
-import { PdfContentComponent } from './components/pdf-content/pdf-content.component';
-import { ChaptersTreeComponent } from './components/chapters-tree/chapters-tree.component';
-import { RenameBookModalComponent } from './components/rename-book-modal/rename-book-modal.component';
-import { DeleteBookModalComponent } from './components/delete-book-modal/delete-book-modal.component';
-import { LogoutModalComponent } from './components/logout-modal/logout-modal.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
    declarations: [
+		// Components
 		AppComponent,
+		EpubContentComponent,
+		PdfContentComponent,
+		ChaptersTreeComponent,
+		RenameBookModalComponent,
+		DeleteBookModalComponent,
+		LogoutModalComponent,
+		// Pages
       LibraryPageComponent,
       BookPageComponent,
       AccountPageComponent,
@@ -57,13 +73,7 @@ import { environment } from '../environments/environment';
       NewAppPageComponent,
       AuthorPageComponent,
       LoginPageComponent,
-      LoadingPageComponent,
-      EpubContentComponent,
-      PdfContentComponent,
-      ChaptersTreeComponent,
-      RenameBookModalComponent,
-      DeleteBookModalComponent,
-		LogoutModalComponent
+      LoadingPageComponent
   	],
   	imports: [
    	AngularReactBrowserModule,
@@ -91,7 +101,8 @@ import { environment } from '../environments/environment';
       ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   	],
   	providers: [
-      DataService
+		DataService,
+		WebsocketService
    ],
    bootstrap: [AppComponent],
    entryComponents: [
