e77f29dc2fc05882550f162bf22390e0edea7782
David <dav2070@web.de>
2019-11-01 23:52:25 +0100

    Implement WebsocketService

---
+++ src/app/services/websocket-service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from "@angular/core";
+declare var io: any;
+
+@Injectable()
+export class WebsocketService{
+	private socket: any;
+	private subscriptions: WebsocketSubscription[] = [];
+	private counter: number = 0;
+
+	constructor(){
+		this.socket = io();
+		
+		for(let key of Object.keys(Callbacks)){
+			this.socket.on(key, (message: any) => {
+				for(let subscription of this.subscriptions){
+					if(subscription.type == +Callbacks[key]) subscription.callback(message);
+				}
+			});
+		}
+	}
+
+	Subscribe(type: WebsocketCallbackType, callback: Function) : number{
+		let key = this.counter++;
+
+		this.subscriptions.push({
+			key,
+			type,
+			callback
+		});
+
+		return key;
+	}
+
+	Unsubscribe(...keys: number[]){
+		for(let key of keys){
+			let i = this.subscriptions.findIndex(c => c.key == key);
+
+			if(i !== -1){
+				this.subscriptions.splice(i, 1);
+			}
+		}
+	}
+
+	Emit(type: WebsocketCallbackType, message: any){
+		let key = getKeyByValue(Callbacks, type);
+
+		if(key) this.socket.emit(key, message);
+	}
+}
+
+interface WebsocketSubscription{
+	key: number;
+	type: WebsocketCallbackType;
+	callback: Function;
+}
+
+export enum WebsocketCallbackType{
+	Login = 1,
+	GetApp = 2
+}
+
+export const Callbacks = {
+	login: WebsocketCallbackType.Login,
+	getApp: WebsocketCallbackType.GetApp
+}
+
+function getKeyByValue(object: any, value: any) {
+	return Object.keys(object).find(key => object[key] === value);
+}
