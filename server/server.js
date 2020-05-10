c70392f8aab97b64752c6b5fe6d2bba393d7b3cb
David <dav2070@web.de>
2020-05-10 16:17:37 +0200

    Remove websocket connection and socket.io package

--- server/server.js
+++ server/server.js
@@ -2,11 +2,8 @@ var path = require('path');
 var express = require('express');
 require('dotenv').config();
 
-var websocket = require('./websocket');
-
 var app = express();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
 
 function getRoot(request, response) {
 	response.sendFile(path.resolve('./PocketLib/index.html'));
@@ -20,9 +17,5 @@ app.use(express.static('./PocketLib'));
 
 app.get('/', getRoot);
 app.get('/*', getUndefined);
-
-io.on('connection', (socket) => {
-	websocket.init(socket);
-});
 
 http.listen(process.env.PORT || 3001);
