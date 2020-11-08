a79f026d06cc15b7a0d7c424b5a17ea458adaaed
David <dav2070@web.de>
2020-11-08 21:25:18 +0100

    Update horizontal-book-list with cover blurhash

--- src/app/components/horizontal-book-list/horizontal-book-list.component.ts
+++ src/app/components/horizontal-book-list/horizontal-book-list.component.ts
@@ -1,49 +1,51 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { ApiResponse } from 'dav-npm';
-import { DataService, GetStoreBookCoverLink } from 'src/app/services/data-service';
-import { ApiService } from 'src/app/services/api-service';
-import { enUS } from 'src/locales/locales';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+import { ApiResponse } from 'dav-npm'
+import { DataService, GetStoreBookCoverLink } from 'src/app/services/data-service'
+import { ApiService } from 'src/app/services/api-service'
+import { enUS } from 'src/locales/locales'
 
 @Component({
 	selector: 'pocketlib-horizontal-book-list',
 	templateUrl: './horizontal-book-list.component.html'
 })
 export class HorizontalBookListComponent{
-	locale = enUS.horizontalBookList;
+	locale = enUS.horizontalBookList
 	books: {
 		uuid: string,
 		title: string,
 		cover: boolean,
-		coverContent: string
-	}[] = [];
-	hoveredBookIndex: number = -1;
+		coverContent: string,
+		coverBlurhash: string
+	}[] = []
+	hoveredBookIndex: number = -1
 	
 	constructor(
 		public dataService: DataService,
 		private apiService: ApiService,
 		private router: Router
 	){
-		this.locale = this.dataService.GetLocale().horizontalBookList;
+		this.locale = this.dataService.GetLocale().horizontalBookList
 	}
 
 	async ngOnInit(){
 		// Get the latest store books
-		this.books = [];
-		let response: ApiResponse<any> = await this.apiService.GetLatestStoreBooks({language: this.dataService.locale.slice(0, 2)});
-		if(response.status != 200) return;
+		this.books = []
+		let response: ApiResponse<any> = await this.apiService.GetLatestStoreBooks({language: this.dataService.locale.slice(0, 2)})
+		if (response.status != 200) return
 		
 		for(let storeBook of response.data.books){
 			this.books.push({
 				uuid: storeBook.uuid,
 				title: storeBook.title,
 				cover: storeBook.cover,
-				coverContent: GetStoreBookCoverLink(storeBook.uuid)
-			});
+				coverContent: GetStoreBookCoverLink(storeBook.uuid),
+				coverBlurhash: storeBook.cover_blurhash
+			})
 		}
 	}
 
 	NavigateToStoreBook(uuid: string){
-		this.router.navigate(["store", "book", uuid]);
+		this.router.navigate(["store", "book", uuid])
 	}
 }
