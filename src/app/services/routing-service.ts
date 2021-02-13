46a8130920c5910f7ad17b2749165e68ff613fe4
David <dav2070@web.de>
2021-02-13 22:53:13 +0100

    Update RoutingService to use access token

--- src/app/services/routing-service.ts
+++ src/app/services/routing-service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { DataService } from './data-service';
+import { Injectable } from '@angular/core'
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router'
+import { DataService } from './data-service'
 
 @Injectable()
-export class RoutingService{
+export class RoutingService {
 	private history: string[] = [];
 	public showsStore: boolean = false;
 	public toolbarNavigationEvent: Function;
@@ -12,62 +12,61 @@ export class RoutingService{
 		private dataService: DataService,
 		private router: Router,
 		private activatedRoute: ActivatedRoute
-	){
-		// Log the user in if there is a jwt in the url
-      this.activatedRoute.queryParams.subscribe(async params => {
-			if (params["jwt"]) {
-            // Login with the jwt
-            if(await this.dataService.user.Login(params["jwt"])){
-               window.location.href = this.router.url.slice(0, this.router.url.indexOf('?'));
-            }
-         }
-		});
+	) {
+		// Log the user in if there is an access token in the url
+		this.activatedRoute.queryParams.subscribe(async params => {
+			if (params["accessToken"]) {
+				// Login with the access token
+				await this.dataService.dav.Login(params["accessToken"])
+				window.location.href = this.router.url.slice(0, this.router.url.indexOf('?'))
+			}
+		})
 
 		this.router.events.subscribe(event => {
-			if(event instanceof NavigationEnd){
-				this.showsStore = event.url.startsWith('/store');	
-				this.history.push(event.url);
+			if (event instanceof NavigationEnd) {
+				this.showsStore = event.url.startsWith('/store')
+				this.history.push(event.url)
 			}
-		});
+		})
 	}
 
-	NavigateBack(alternativeRoute: string){
+	NavigateBack(alternativeRoute: string) {
 		// Remove the current url
-		this.history.pop();
-		
+		this.history.pop()
+
 		// Navigate to the last url or the alternative route
-		if(this.history.length > 0){
-			this.router.navigateByUrl(this.history.pop());
-		}else{
-			this.router.navigateByUrl(alternativeRoute);
+		if (this.history.length > 0) {
+			this.router.navigateByUrl(this.history.pop())
+		} else {
+			this.router.navigateByUrl(alternativeRoute)
 		}
 	}
 
 	NavigateToLibraryPage() {
-		this.NavigateToPage(["/"]);
+		this.NavigateToPage(["/"])
 	}
 
 	NavigateToAuthorPage() {
-		this.NavigateToPage(["author"]);
+		this.NavigateToPage(["author"])
 	}
 
 	NavigateToStorePage() {
-		this.NavigateToPage(["store"]);
+		this.NavigateToPage(["store"])
 	}
 
 	NavigateToAccountPage() {
-		this.NavigateToPage(["account"]);
+		this.NavigateToPage(["account"])
 	}
 
 	NavigateToSettingsPage() {
-		this.NavigateToPage(["settings"]);
+		this.NavigateToPage(["settings"])
 	}
 
 	private async NavigateToPage(route: string[]) {
 		if (this.toolbarNavigationEvent) {
-			if(await this.toolbarNavigationEvent()) this.router.navigate(route);
+			if (await this.toolbarNavigationEvent()) this.router.navigate(route)
 		} else {
-			this.router.navigate(route);
+			this.router.navigate(route)
 		}
 	}
 }
