c53cd2e93e06863768a5094dbf04e614e080b7dd
David <dav2070@web.de>
2021-04-22 17:05:54 +0200

    Format and clean up the code in AppRouting module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LibraryPageComponent } from './pages/library-page/library-page.component';
-import { BookPageComponent } from './pages/book-page/book-page.component';
-import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
-import { AccountPageComponent } from './pages/account-page/account-page.component';
-import { AuthorPageComponent } from './pages/author-page/author-page.component';
-import { AuthorSetupPageComponent } from './pages/author-setup-page/author-setup-page.component';
-import { AuthorCollectionPageComponent } from './pages/author-collection-page/author-collection-page.component';
-import { AuthorBookPageComponent } from './pages/author-book-page/author-book-page.component';
-import { NewBookPageComponent } from './pages/new-book-page/new-book-page.component';
-import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
-import { StorePageComponent } from './pages/store-page/store-page.component';
-import { StoreStartPageComponent } from './pages/store-start-page/store-start-page.component';
-import { StoreAuthorPageComponent } from './pages/store-author-page/store-author-page.component';
-import { StoreBookPageComponent } from './pages/store-book-page/store-book-page.component';
-import { StoreBooksPageComponent } from './pages/store-books-page/store-books-page.component';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { LibraryPageComponent } from './pages/library-page/library-page.component'
+import { BookPageComponent } from './pages/book-page/book-page.component'
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component'
+import { AccountPageComponent } from './pages/account-page/account-page.component'
+import { AuthorPageComponent } from './pages/author-page/author-page.component'
+import { AuthorSetupPageComponent } from './pages/author-setup-page/author-setup-page.component'
+import { AuthorCollectionPageComponent } from './pages/author-collection-page/author-collection-page.component'
+import { AuthorBookPageComponent } from './pages/author-book-page/author-book-page.component'
+import { NewBookPageComponent } from './pages/new-book-page/new-book-page.component'
+import { LoadingPageComponent } from './pages/loading-page/loading-page.component'
+import { StorePageComponent } from './pages/store-page/store-page.component'
+import { StoreStartPageComponent } from './pages/store-start-page/store-start-page.component'
+import { StoreAuthorPageComponent } from './pages/store-author-page/store-author-page.component'
+import { StoreBookPageComponent } from './pages/store-book-page/store-book-page.component'
+import { StoreBooksPageComponent } from './pages/store-books-page/store-books-page.component'
 
 const routes: Routes = [
-   { path: "", component: LibraryPageComponent },
-   { path: "book", component: BookPageComponent },
+	{ path: "", component: LibraryPageComponent },
+	{ path: "book", component: BookPageComponent },
 	{ path: "account", component: AccountPageComponent },
-   { path: "settings", component: SettingsPageComponent },
+	{ path: "settings", component: SettingsPageComponent },
 	{ path: "author", component: AuthorPageComponent },
 	{ path: "author/setup", component: AuthorSetupPageComponent },
 	{ path: "author/:uuid", component: AuthorPageComponent },
@@ -28,16 +28,18 @@ const routes: Routes = [
 	{ path: "author/book/new", component: NewBookPageComponent },
 	{ path: "author/book/:uuid", component: AuthorBookPageComponent },
 	{ path: "loading", component: LoadingPageComponent },
-	{ path: "store", component: StorePageComponent, children: [
-		{ path: "", component: StoreStartPageComponent },
-		{ path: "author/:uuid", component: StoreAuthorPageComponent },
-		{ path: "book/:uuid", component: StoreBookPageComponent },
-		{ path: "books/:key", component: StoreBooksPageComponent }
-	]}
+	{
+		path: "store", component: StorePageComponent, children: [
+			{ path: "", component: StoreStartPageComponent },
+			{ path: "author/:uuid", component: StoreAuthorPageComponent },
+			{ path: "book/:uuid", component: StoreBookPageComponent },
+			{ path: "books/:key", component: StoreBooksPageComponent }
+		]
+	}
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
