f75d3b41c16721d1dace490946d3ebe1f54098b8
David <dav2070@web.de>
2022-03-10 19:36:03 +0100

    Upgrade to dav-js v2.3 and dav-ui-components v0.8.3 & use staging backend in staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	apiKey: "gHgHKRbIjdguCM4cv5481hdiF5hZGWZ4x12Ur-7v",
 	baseUrl: "https://pocketlib-staging-d9rk6.ondigitalocean.app",
-	pocketlibApiBaseUrl: "https://dav-backend-tfpik.ondigitalocean.app/v1/api/1/dev/call",
+	pocketlibApiBaseUrl: "https://dav-backend-tfpik.ondigitalocean.app/staging/v1/api/1/dev/call",
 	websiteBaseUrl: "https://dav-apps.tech",
 	appId: 6,
 	bookTableId: 14,
