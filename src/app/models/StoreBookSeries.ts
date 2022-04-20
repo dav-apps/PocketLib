ed5a1083fcfea46821c0536c4706ed9971ea2cbb
David <dav2070@web.de>
2022-04-20 15:51:53 +0200

    Update StoreBookSeries class for StoreBooks instead of collections and other changes

--- src/app/models/StoreBookSeries.ts
+++ src/app/models/StoreBookSeries.ts
@@ -2,139 +2,87 @@ import { ApiResponse, isSuccessStatusCode, PromiseHolder } from "dav-js"
 import {
 	ListResponseData,
 	Language,
-	StoreBookCollectionResource,
-	StoreBookCollectionListField,
 	StoreBookSeriesResource,
-	StoreBookSeriesNameResource,
-	StoreBookSeriesNameListField
+	StoreBookResource,
+	StoreBookListField
 } from "../misc/types"
 import { GetAllLanguages, GetLanguageByString } from "../misc/utils"
 import { ApiService } from "../services/api-service"
-import { CachingService } from "../services/caching-service"
-import { StoreBookCollection } from "./StoreBookCollection"
+import { StoreBook } from "./StoreBook"
 
 export class StoreBookSeries {
 	public uuid: string
 	public author: string
-	public name: {
-		value: string
-		language: Language
-	}
-	private names: {
+	public name: string
+	public language: Language
+	private storeBooks: {
 		loaded: boolean
 		isLoading: boolean
-		itemsPromiseHolder: PromiseHolder<StoreBookSeriesNameResource[]>
-	}
-	private collections: {
-		loaded: boolean
-		isLoading: boolean
-		itemsPromiseHolder: PromiseHolder<StoreBookCollectionResource[]>
+		itemsPromiseHolder: PromiseHolder<StoreBookResource[]>
 	}
 
 	constructor(
 		seriesResource: StoreBookSeriesResource,
-		private apiService: ApiService,
-		private cachingService: CachingService
+		private apiService: ApiService
 	) {
 		this.uuid = seriesResource?.uuid ?? ""
 		this.author = seriesResource?.author ?? ""
-		this.name = {
-			value: seriesResource?.name?.value ?? "",
-			language: GetLanguageByString(seriesResource?.name?.language)
-		}
-		this.names = { loaded: false, isLoading: false, itemsPromiseHolder: new PromiseHolder() }
-		this.collections = { loaded: false, isLoading: false, itemsPromiseHolder: new PromiseHolder() }
+		this.name = seriesResource?.name ?? ""
+		this.language = GetLanguageByString(seriesResource?.language)
+		this.storeBooks = { loaded: false, isLoading: false, itemsPromiseHolder: new PromiseHolder() }
 	}
 
-	async GetNames(): Promise<StoreBookSeriesNameResource[]> {
-		if (this.names.isLoading || this.names.loaded) {
+	async GetStoreBooks(): Promise<StoreBook[]> {
+		if (this.storeBooks.isLoading || this.storeBooks.loaded) {
 			let items = []
 
-			for (let item of await this.names.itemsPromiseHolder.AwaitResult()) {
+			for (let item of await this.storeBooks.itemsPromiseHolder.AwaitResult()) {
 				items.push(item)
 			}
 
 			return items
 		}
 
-		this.names.isLoading = true
-		this.names.itemsPromiseHolder.Setup()
+		this.storeBooks.isLoading = true
+		this.storeBooks.itemsPromiseHolder.Setup()
 
-		// Get the names of the series
-		let response = await this.apiService.ListStoreBookSeriesNames({
-			uuid: this.uuid,
-			fields: [
-				StoreBookSeriesNameListField.items_uuid,
-				StoreBookSeriesNameListField.items_name,
-				StoreBookSeriesNameListField.items_language
-			]
-		})
-
-		if (!isSuccessStatusCode(response.status)) {
-			this.names.isLoading = false
-			this.names.itemsPromiseHolder.Resolve([])
-			return []
-		}
-
-		this.names.loaded = true
-		this.names.isLoading = false
-		let responseData = (response as ApiResponse<ListResponseData<StoreBookSeriesNameResource>>).data
-		let items = []
-
-		for (let item of responseData.items) {
-			items.push(item)
-		}
-
-		this.names.itemsPromiseHolder.Resolve(items)
-		return items
-	}
-
-	ClearNames() {
-		this.names.loaded = false
-		this.cachingService.ClearApiRequestCache(this.apiService.ListStoreBookSeriesNames.name)
-	}
-
-	async GetCollections(): Promise<StoreBookCollection[]> {
-		if (this.collections.isLoading || this.collections.loaded) {
-			let items = []
-
-			for (let item of await this.collections.itemsPromiseHolder.AwaitResult()) {
-				items.push(item)
-			}
-
-			return items
-		}
-
-		this.collections.isLoading = true
-		this.collections.itemsPromiseHolder.Setup()
-
-		// Get the collections of the series
-		let response = await this.apiService.ListStoreBookCollections({
+		// Get the store books of the series
+		let response = await this.apiService.ListStoreBooks({
 			series: this.uuid,
 			fields: [
-				StoreBookCollectionListField.items_uuid,
-				StoreBookCollectionListField.items_author,
-				StoreBookCollectionListField.items_name
+				StoreBookListField.items_uuid,
+				StoreBookListField.items_collection,
+				StoreBookListField.items_title,
+				StoreBookListField.items_description,
+				StoreBookListField.items_language,
+				StoreBookListField.items_price,
+				StoreBookListField.items_isbn,
+				StoreBookListField.items_status,
+				StoreBookListField.items_cover,
+				StoreBookListField.items_file,
+				StoreBookListField.items_inLibrary,
+				StoreBookListField.items_purchased,
+				StoreBookListField.items_categories
 			],
 			languages: GetAllLanguages()
 		})
 
 		if (!isSuccessStatusCode(response.status)) {
-			this.collections.isLoading = false
-			this.collections.itemsPromiseHolder.Resolve([])
+			this.storeBooks.isLoading = false
+			this.storeBooks.itemsPromiseHolder.Resolve([])
 			return []
 		}
 
-		this.collections.loaded = true
-		this.collections.isLoading = false
-		let responseData = (response as ApiResponse<ListResponseData<StoreBookCollectionResource>>).data
+		this.storeBooks.loaded = true
+		this.storeBooks.isLoading = false
+		let responseData = (response as ApiResponse<ListResponseData<StoreBookResource>>).data
 		let items = []
 
 		for (let item of responseData.items) {
-			items.push(new StoreBookCollection(item, this.apiService))
+			items.push(new StoreBook(item, this.apiService))
 		}
 
-		this.collections.itemsPromiseHolder.Resolve(items)
+		this.storeBooks.itemsPromiseHolder.Resolve(items)
 		return items
 	}
 }
