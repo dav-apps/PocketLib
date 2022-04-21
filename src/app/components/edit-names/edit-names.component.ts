f90c719b8e2c95561a3c2196b434bd8ad4bb930d
David <dav2070@web.de>
2022-04-21 16:08:54 +0200

    Remove ability to add names in new languages in EditNames component

--- src/app/components/edit-names/edit-names.component.ts
+++ src/app/components/edit-names/edit-names.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, Output, EventEmitter, SimpleChanges } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { ApiErrorResponse, isSuccessStatusCode } from 'dav-js'
-import { DropdownOption, DropdownOptionType } from 'dav-ui-components'
 import { DataService } from 'src/app/services/data-service'
 import { ApiService } from 'src/app/services/api-service'
 import * as ErrorCodes from 'src/constants/errorCodes'
@@ -23,96 +22,12 @@ export class EditNamesComponent {
 	@Input() names: Name[] = []
 	@Input() uuid: string
 	@Output() update = new EventEmitter()
-	addLanguageSelectedKey: string = "default"
-	addLanguageOptions: DropdownOption[] = []
-	newLanguageName: string = ""
-	newLanguageNameError: string = ""
 
 	constructor(
 		private dataService: DataService,
 		private apiService: ApiService
 	) {
 		this.locale = this.dataService.GetLocale().editNames
-	}
-
-	ngOnInit() {
-		this.LoadLanguageOptions()
-	}
-
-	ngOnChanges(changes: SimpleChanges) {
-		if (changes.names != null) {
-			this.LoadLanguageOptions()
-		}
-	}
-
-	LoadLanguageOptions() {
-		this.addLanguageOptions = [{
-			key: "default",
-			value: this.locale.selectLanguage,
-			type: DropdownOptionType.option
-		}]
-
-		let languages = this.dataService.GetLocale().misc.languages
-
-		for (let language of Object.keys(languages)) {
-			if (this.names.findIndex(name => name.language == language) == -1) {
-				// Add the language as an option to add
-				this.addLanguageOptions.push({
-					key: language,
-					value: languages[language],
-					type: DropdownOptionType.option
-				})
-			}
-		}
-	}
-
-	async AddLanguage() {
-		// Find the selected option
-		let i = this.addLanguageOptions.findIndex(option => option.key == this.addLanguageSelectedKey)
-		if (i == -1) return
-
-		// Create the name on the server
-		let setNameResponse = await this.apiService.SetStoreBookCollectionName({
-			uuid: this.uuid,
-			language: this.addLanguageSelectedKey,
-			name: this.newLanguageName
-		})
-
-		if (isSuccessStatusCode(setNameResponse.status)) {
-			this.names.push({
-				name: this.newLanguageName,
-				language: this.addLanguageSelectedKey,
-				fullLanguage: this.addLanguageOptions[i].value,
-				edit: false,
-				errorMessage: ""
-			})
-
-			this.update.emit({
-				name: this.newLanguageName,
-				language: this.addLanguageSelectedKey
-			})
-
-			// Remove the selected option and reset the dropdown
-			this.addLanguageOptions.splice(i, 1)
-			this.addLanguageSelectedKey = "default"
-		} else {
-			let errorCode = (setNameResponse as ApiErrorResponse).errors[0].code
-
-			switch (errorCode) {
-				case ErrorCodes.NameMissing:
-					this.newLanguageNameError = this.locale.errors.nameMissing
-					break
-				case ErrorCodes.NameTooShort:
-					this.newLanguageNameError = this.locale.errors.nameTooShort
-					break
-				case ErrorCodes.NameTooLong:
-					this.newLanguageNameError = this.locale.errors.nameTooLong
-					break
-				default:
-					this.newLanguageNameError = this.locale.errors.unexpectedError
-					break
-			}
-		}
 	}
 
 	async UpdateName(name: Name) {
@@ -150,9 +65,5 @@ export class EditNamesComponent {
 					break
 			}
 		}
-	}
-
-	AddLanguageDropdownChange(event: CustomEvent) {
-		this.addLanguageSelectedKey = event.detail.key
 	}
 }
