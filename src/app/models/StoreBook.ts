149155af5250e0ea111e60d528d2f53db59f0f9b
David <dav2070@web.de>
2022-04-25 01:55:10 +0200

    Add GetReleases function to StoreBook class

--- src/app/models/StoreBook.ts
+++ src/app/models/StoreBook.ts
@@ -1,5 +1,12 @@
-import { ApiResponse, isSuccessStatusCode } from "dav-js"
-import { BookStatus, Language, StoreBookResource } from "../misc/types"
+import { ApiResponse, isSuccessStatusCode, PromiseHolder } from "dav-js"
+import {
+	BookStatus,
+	Language,
+	StoreBookResource,
+	StoreBookReleaseResource,
+	StoreBookReleaseListField,
+	ListResponseData
+} from "../misc/types"
 import { GetBookStatusByString, GetLanguageByString } from "../misc/utils"
 import { ApiService } from "../services/api-service"
 
@@ -22,6 +29,11 @@ export class StoreBook {
 	}
 	public categories: string[]
 	private coverContent: string
+	private releases: {
+		loaded: boolean
+		isLoading: boolean
+		itemsPromiseHolder: PromiseHolder<StoreBookReleaseResource[]>
+	}
 
 	constructor(storeBookResource: StoreBookResource, private apiService: ApiService) {
 		this.uuid = storeBookResource.uuid
@@ -41,6 +53,7 @@ export class StoreBook {
 			fileName: storeBookResource.file?.fileName
 		}
 		this.categories = storeBookResource.categories
+		this.releases = { loaded: false, isLoading: false, itemsPromiseHolder: new PromiseHolder() }
 	}
 
 	async GetCoverContent(): Promise<string> {
@@ -56,5 +69,57 @@ export class StoreBook {
 		let responseData = (response as ApiResponse<string>).data
 		this.coverContent = responseData
 		return responseData
+	}
+
+	async GetReleases(): Promise<StoreBookReleaseResource[]> {
+		if (this.releases.isLoading || this.releases.loaded) {
+			let items = []
+
+			for (let item of await this.releases.itemsPromiseHolder.AwaitResult()) {
+				items.push(item)
+			}
+
+			return items
+		}
+
+		this.releases.isLoading = true
+		this.releases.itemsPromiseHolder.Setup()
+
+		// Get the releases of the store book
+		let response = await this.apiService.ListStoreBookReleases({
+			storeBook: this.uuid,
+			fields: [
+				StoreBookReleaseListField.items_uuid,
+				StoreBookReleaseListField.items_storeBook,
+				StoreBookReleaseListField.items_releaseName,
+				StoreBookReleaseListField.items_releaseNotes,
+				StoreBookReleaseListField.items_title,
+				StoreBookReleaseListField.items_description,
+				StoreBookReleaseListField.items_price,
+				StoreBookReleaseListField.items_isbn,
+				StoreBookReleaseListField.items_status,
+				StoreBookReleaseListField.items_cover,
+				StoreBookReleaseListField.items_file,
+				StoreBookReleaseListField.items_categories
+			]
+		})
+
+		if (!isSuccessStatusCode(response.status)) {
+			this.releases.isLoading = false
+			this.releases.itemsPromiseHolder.Resolve([])
+			return []
+		}
+
+		this.releases.loaded = true
+		this.releases.isLoading = false
+		let responseData = (response as ApiResponse<ListResponseData<StoreBookReleaseResource>>).data
+		let items = []
+
+		for (let item of responseData.items) {
+			items.push(item)
+		}
+
+		this.releases.itemsPromiseHolder.Resolve(items)
+		return items
 	}
 }
