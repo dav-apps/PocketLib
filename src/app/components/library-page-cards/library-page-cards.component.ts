cc1ec8e5b41a5b4e19f1a38e1caa96019e7a9db3
David <zurek.dav@gmail.com>
2023-02-17 22:02:27 +0100

    Refactor LibraryPageCards component & use FontAwesome icons

--- src/app/components/library-page-cards/library-page-cards.component.ts
+++ src/app/components/library-page-cards/library-page-cards.component.ts
@@ -1,17 +1,24 @@
 import { Component, Output, EventEmitter } from '@angular/core'
 import { ReadFile } from 'ngx-file-helpers'
-import { faAddressCard } from '@fortawesome/pro-light-svg-icons'
+import {
+	faAddressCard as faAddressCardLight,
+	faPlus as faPlusLight,
+	faShoppingBag as faShoppingBagLight
+} from '@fortawesome/pro-light-svg-icons'
 import { DataService } from 'src/app/services/data-service'
 import { enUS } from 'src/locales/locales'
 
 @Component({
 	selector: 'pocketlib-library-page-cards',
-	templateUrl: './library-page-cards.component.html'
+	templateUrl: './library-page-cards.component.html',
+	styleUrls: ['./library-page-cards.component.scss']
 })
 export class LibraryPageCardsComponent{
 	locale = enUS.libraryPageCards
 	@Output() addBookFilePick = new EventEmitter()
-	faAddressCard = faAddressCard
+	faAddressCardLight = faAddressCardLight
+	faPlusLight = faPlusLight
+	faShoppingBagLight = faShoppingBagLight
 	addBookHover: boolean = false					// Indicator for if the mouse is hovering the add book card
 	discoverBooksHover: boolean = false			// Indicator for if the mouse is hovering the discover books card
 	goToAuthorPageHover: boolean = false		// Indicator for if the mouse is hovering the card for going to the author page
