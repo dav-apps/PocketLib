ec3814ecba4efb09d550c7aa8b5f859906aea495
David <zurek.dav@gmail.com>
2023-02-26 01:02:24 +0100

    Use header component on Account page

--- src/app/pages/account-page/account-page.component.ts
+++ src/app/pages/account-page/account-page.component.ts
@@ -14,7 +14,8 @@ import { GetDualScreenSettings } from "src/app/misc/utils"
 
 @Component({
 	selector: "pocketlib-account-page",
-	templateUrl: "./account-page.component.html"
+	templateUrl: "./account-page.component.html",
+	styleUrls: ["./account-page.component.scss"]
 })
 export class AccountPageComponent {
 	faBagShoppingLight = faBagShoppingLight
