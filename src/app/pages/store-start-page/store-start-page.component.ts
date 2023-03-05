8dfeac0838c42c04900123682c951dcd947053ba
David <zurek.dav@gmail.com>
2023-03-05 22:57:25 +0100

    Center store start page lists

--- src/app/pages/store-start-page/store-start-page.component.ts
+++ src/app/pages/store-start-page/store-start-page.component.ts
@@ -3,7 +3,8 @@ import { DataService } from "src/app/services/data-service"
 
 @Component({
 	selector: "pocketlib-store-start-page",
-	templateUrl: "./store-start-page.component.html"
+	templateUrl: "./store-start-page.component.html",
+	styleUrls: ["./store-start-page.component.scss"]
 })
 export class StoreStartPageComponent {
 	constructor(public dataService: DataService) {}
