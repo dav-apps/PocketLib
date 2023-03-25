ea16c98b20b91cdc8bc29bf8250d2d58965738c5
David <zurek.dav@gmail.com>
2023-03-25 22:33:09 +0100

    Use BlurhashImage of dav-ui-components in HorizontalAuthorListItem component

--- src/app/components/horizontal-author-list-item/horizontal-author-list-item.component.ts
+++ src/app/components/horizontal-author-list-item/horizontal-author-list-item.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, ViewChild } from "@angular/core"
+import { Component, Input } from "@angular/core"
 import { AuthorListItem } from "src/app/misc/types"
 import { DataService } from "src/app/services/data-service"
-import { BlurhashImageComponent } from "../blurhash-image/blurhash-image.component"
 
 @Component({
 	selector: "pocketlib-horizontal-author-list-item",
-	templateUrl: "./horizontal-author-list-item.component.html"
+	templateUrl: "./horizontal-author-list-item.component.html",
+	styleUrls: ["./horizontal-author-list-item.component.scss"]
 })
 export class HorizontalAuthorListItemComponent {
 	@Input() author: AuthorListItem = {
@@ -16,19 +16,6 @@ export class HorizontalAuthorListItemComponent {
 		profileImageBlurhash: null,
 		profileImageAlt: ""
 	}
-	@ViewChild("blurhashImage", { static: true })
-	blurhashImage: BlurhashImageComponent
-	hovered: boolean = false
 
 	constructor(public dataService: DataService) {}
-
-	ShowImageShadow() {
-		this.hovered = true
-		this.blurhashImage.AddShadow()
-	}
-
-	HideImageShadow() {
-		this.hovered = false
-		this.blurhashImage.RemoveShadow()
-	}
 }
