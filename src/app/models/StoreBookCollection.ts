f51f9c6bc5b84570fc4a27b692dc9297d195d0e7
David <zurek.dav@gmail.com>
2023-09-11 20:14:03 +0200

    Rewrite StoreBookCollection class
    - Add static Retrieve function to get a StoreBookCollection object using the uuid
    - Rewrite GetNames and GetStoreBooks functions to get only the uuids of the items and to load the objects using the Retrieve functions of the respective classes

--- src/app/models/StoreBookCollection.ts
+++ src/app/models/StoreBookCollection.ts
@@ -1,174 +1,138 @@
-import { PromiseHolder } from "dav-js"
 import {
-	Language,
 	StoreBookCollectionResource,
-	StoreBookCollectionNameResource
+	StoreBookCollectionNameResource,
+	Language,
+	List
 } from "../misc/types"
 import { GetLanguageByString } from "../misc/utils"
+import { DataService } from "src/app/services/data-service"
 import { ApiService } from "src/app/services/api-service"
-import { StoreBook } from "src/app/models/StoreBook"
+import { StoreBook } from "./StoreBook"
+import { StoreBookCollectionName } from "./StoreBookCollectionName"
 
 export class StoreBookCollection {
 	public uuid: string
-	public author: string
 	public name: {
-		value: string
+		name: string
 		language: Language
-	}
-	private names: {
-		loaded: boolean
-		isLoading: boolean
-		itemsPromiseHolder: PromiseHolder<StoreBookCollectionNameResource[]>
-	}
-	private storeBooks: {
-		loaded: boolean
-		isLoading: boolean
-		itemsPromiseHolder: PromiseHolder<StoreBook[]>
 	}
 
 	constructor(
 		collectionResource: StoreBookCollectionResource,
+		private dataService: DataService,
 		private apiService: ApiService
 	) {
-		this.uuid = collectionResource?.uuid ?? ""
-		this.author = collectionResource?.author?.uuid ?? ""
-		this.name = {
-			value: collectionResource?.name?.name ?? "",
-			language: GetLanguageByString(collectionResource?.name?.language)
-		}
-		this.names = {
-			loaded: false,
-			isLoading: false,
-			itemsPromiseHolder: new PromiseHolder()
-		}
-		this.storeBooks = {
-			loaded: false,
-			isLoading: false,
-			itemsPromiseHolder: new PromiseHolder()
+		if (collectionResource != null) {
+			if (collectionResource.uuid != null)
+				this.uuid = collectionResource.uuid
+			this.name = {
+				name: collectionResource.name?.name,
+				language: GetLanguageByString(collectionResource.name?.language)
+			}
 		}
 	}
 
-	async GetNames(): Promise<StoreBookCollectionNameResource[]> {
-		if (this.names.isLoading || this.names.loaded) {
-			let items = []
-
-			for (let item of await this.names.itemsPromiseHolder.AwaitResult()) {
-				items.push(item)
-			}
-
-			return items
-		}
-
-		this.names.isLoading = true
-		this.names.itemsPromiseHolder.Setup()
-
-		// Get the names of the collection
-		let response = await this.apiService.retrieveStoreBookCollection(
+	static async Retrieve(
+		uuid: string,
+		dataService: DataService,
+		apiService: ApiService
+	): Promise<StoreBookCollection> {
+		let response = await apiService.retrieveStoreBookCollection(
 			`
-				names {
-					items {
-						uuid
-						name
-						language
-					}
+				name(languages: $languages) {
+					name
+					language
 				}
 			`,
-			{ uuid: this.uuid }
+			{
+				uuid,
+				languages: await dataService.GetStoreLanguages()
+			}
 		)
 
 		let responseData = response.data.retrieveStoreBookCollection
+		if (responseData == null) return null
 
-		if (responseData == null) {
-			this.names.isLoading = false
-			this.names.itemsPromiseHolder.Resolve([])
-			return []
-		}
+		return new StoreBookCollection(
+			{ uuid, ...responseData },
+			dataService,
+			apiService
+		)
+	}
 
-		this.names.loaded = true
-		this.names.isLoading = false
+	async GetNames(params?: {
+		limit?: number
+		offset?: number
+	}): Promise<List<StoreBookCollectionNameResource>> {
+		let response = await this.apiService.retrieveStoreBookCollection(
+			`
+				names(limit: $limit, offset: $offset) {
+					items {
+						uuid
+					}
+				}
+			`,
+			{
+				uuid: this.uuid,
+				limit: params.limit,
+				offset: params.offset
+			}
+		)
+
+		let responseData = response.data.retrieveStoreBookCollection
+		if (responseData == null) return { total: 0, items: [] }
+
 		let items = []
 
 		for (let item of responseData.names.items) {
-			items.push(item)
+			items.push(
+				await StoreBookCollectionName.Retrieve(item.uuid, this.apiService)
+			)
 		}
 
-		this.names.itemsPromiseHolder.Resolve(items)
-		return items
-	}
-
-	ClearNames() {
-		this.names.loaded = false
-	}
-
-	async GetStoreBooks(): Promise<StoreBook[]> {
-		if (this.storeBooks.isLoading || this.storeBooks.loaded) {
-			let items = []
-
-			for (let item of await this.storeBooks.itemsPromiseHolder.AwaitResult()) {
-				items.push(item)
-			}
-
-			return items
+		return {
+			total: responseData.names.total,
+			items
 		}
+	}
 
-		this.storeBooks.isLoading = true
-		this.storeBooks.itemsPromiseHolder.Setup()
-
-		// Get the store books of the collection
+	async GetStoreBooks(params?: {
+		limit?: number
+		offset?: number
+	}): Promise<List<StoreBook>> {
 		let response = await this.apiService.retrieveStoreBookCollection(
 			`
 				storeBooks {
 					items {
 						uuid
-						collection {
-							uuid
-						}
-						title
-						description
-						language
-						price
-						isbn
-						status
-						cover {
-							uuid
-						}
-						file {
-							uuid
-						}
-						inLibrary
-						purchased
-						categories {
-							items {
-								key
-							}
-						}
 					}
 				}
 			`,
-			{ uuid: this.uuid }
+			{
+				uuid: this.uuid,
+				limit: params?.limit,
+				offset: params?.offset
+			}
 		)
 
 		let responseData = response.data.retrieveStoreBookCollection
+		if (responseData == null) return { total: 0, items: [] }
 
-		if (responseData == null) {
-			this.storeBooks.isLoading = false
-			this.storeBooks.itemsPromiseHolder.Resolve([])
-			return []
-		}
-
-		this.storeBooks.loaded = true
-		this.storeBooks.isLoading = false
 		let items = []
 
 		for (let item of responseData.storeBooks.items) {
-			items.push(new StoreBook(item, this.apiService))
+			items.push(
+				await StoreBook.Retrieve(
+					item.uuid,
+					this.dataService,
+					this.apiService
+				)
+			)
 		}
 
-		this.storeBooks.itemsPromiseHolder.Resolve(items)
-		return items
-	}
-
-	ClearStoreBooks() {
-		this.storeBooks.loaded = false
+		return {
+			total: responseData.storeBooks.total,
+			items
+		}
 	}
 }
