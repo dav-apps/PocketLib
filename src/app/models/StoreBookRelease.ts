ba81e505f9ea1cb2c31949cfa3df368fdc6f268f
David <zurek.dav@gmail.com>
2023-09-11 20:16:55 +0200

    Rewrite StoreBookRelease class
    - Add static Retrieve function to get a StoreBookRelease object using the uuid
    - Add GetCategories function

--- src/app/models/StoreBookRelease.ts
+++ src/app/models/StoreBookRelease.ts
@@ -2,14 +2,16 @@ import { isSuccessStatusCode } from "dav-js"
 import {
 	StoreBookReleaseStatus,
 	ApiResponse,
+	List,
 	StoreBookReleaseResource
 } from "src/app/misc/types"
 import { GetStoreBookReleaseStatusByString } from "../misc/utils"
 import { ApiService } from "../services/api-service"
+import { DataService } from "../services/data-service"
+import { Category } from "./Category"
 
 export class StoreBookRelease {
 	public uuid: string
-	public storeBook: string
 	public releaseName: string
 	public releaseNotes: string
 	public title: string
@@ -25,36 +27,70 @@ export class StoreBookRelease {
 	public file: {
 		fileName: string
 	}
-	public categories: string[]
 	private coverContent: string
 
 	constructor(
-		storeBookRelease: StoreBookReleaseResource,
+		releaseResource: StoreBookReleaseResource,
+		private dataService: DataService,
 		private apiService: ApiService
 	) {
-		this.uuid = storeBookRelease.uuid
-		this.storeBook = storeBookRelease.storeBook?.uuid
-		this.releaseName = storeBookRelease.releaseName
-		this.releaseNotes = storeBookRelease.releaseNotes
-		this.title = storeBookRelease.title
-		this.description = storeBookRelease.description
-		this.price = storeBookRelease.price
-		this.isbn = storeBookRelease.isbn
-		this.status = GetStoreBookReleaseStatusByString(storeBookRelease.status)
-		this.cover = {
-			url: storeBookRelease.cover?.url,
-			aspectRatio: storeBookRelease.cover?.aspectRatio,
-			blurhash: storeBookRelease.cover?.blurhash
+		if (releaseResource != null) {
+			if (releaseResource.uuid != null) this.uuid = releaseResource.uuid
+			if (releaseResource.releaseName != null)
+				this.releaseName = releaseResource.releaseName
+			if (releaseResource.releaseNotes != null)
+				this.releaseNotes = releaseResource.releaseNotes
+			if (releaseResource.title != null) this.title = releaseResource.title
+			if (releaseResource.description != null)
+				this.description = releaseResource.description
+			if (releaseResource.price != null) this.price = releaseResource.price
+			if (releaseResource.isbn != null) this.isbn = releaseResource.isbn
+			if (releaseResource.status != null)
+				this.status = GetStoreBookReleaseStatusByString(
+					releaseResource.status
+				)
+			this.cover = {
+				url: releaseResource.cover?.url,
+				aspectRatio: releaseResource.cover?.aspectRatio,
+				blurhash: releaseResource.cover?.blurhash
+			}
+			this.file = {
+				fileName: releaseResource.file?.fileName
+			}
 		}
-		this.file = {
-			fileName: storeBookRelease.file?.fileName
-		}
+	}
 
-		this.categories = []
+	static async Retrieve(
+		uuid: string,
+		dataService: DataService,
+		apiService: ApiService
+	): Promise<StoreBookRelease> {
+		let response = await apiService.retrieveStoreBookRelease(
+			`
+				uuid
+				releaseName
+				releaseNotes
+				title
+				description
+				price
+				isbn
+				status
+				cover {
+					url
+					aspectRatio
+					blurhash
+				}
+				file {
+					fileName
+				}
+			`,
+			{ uuid }
+		)
 
-		for (let category of storeBookRelease.categories.items) {
-			this.categories.push(category.key)
-		}
+		let responseData = response.data.retrieveStoreBookRelease
+		if (responseData == null) return null
+
+		return new StoreBookRelease(responseData, dataService, apiService)
 	}
 
 	async GetCoverContent(): Promise<string> {
@@ -70,5 +106,38 @@ export class StoreBookRelease {
 		let responseData = (response as ApiResponse<string>).data
 		this.coverContent = responseData
 		return responseData
+	}
+
+	async GetCategories(): Promise<List<Category>> {
+		let response = await this.apiService.retrieveStoreBookRelease(
+			`
+				categories(limit: $limit, offset: $offset) {
+					items {
+						uuid
+					}
+				}
+			`,
+			{ uuid: this.uuid }
+		)
+
+		let responseData = response.data.retrieveStoreBookRelease
+		if (responseData == null) return { total: 0, items: [] }
+
+		let items = []
+
+		for (let item of responseData.categories.items) {
+			items.push(
+				await Category.Retrieve(
+					item.uuid,
+					this.dataService,
+					this.apiService
+				)
+			)
+		}
+
+		return {
+			total: responseData.categories.total,
+			items
+		}
 	}
 }
