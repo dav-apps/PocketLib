e4c2a42cc7eb722a6f3993fcc40419db3528c8df
David <zurek.dav@gmail.com>
2023-09-13 18:06:55 +0200

    Redesign file section of NewBookPage
    - Replace bootstrap utility classes with CSS styles

--- src/app/components/new-book-page-book-file-section/new-book-page-book-file-section.component.ts
+++ src/app/components/new-book-page-book-file-section/new-book-page-book-file-section.component.ts
@@ -5,7 +5,8 @@ import { enUS } from "src/locales/locales"
 
 @Component({
 	selector: "pocketlib-new-book-page-book-file-section",
-	templateUrl: "./new-book-page-book-file-section.component.html"
+	templateUrl: "./new-book-page-book-file-section.component.html",
+	styleUrls: ["./new-book-page-book-file-section.component.scss"]
 })
 export class NewBookPageBookFileSectionComponent {
 	locale = enUS.newBookPage
