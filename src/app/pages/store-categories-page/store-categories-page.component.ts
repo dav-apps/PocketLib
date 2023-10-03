2bd7092810023d449046169592fa332199ab962e
David <zurek.dav@gmail.com>
2023-10-03 15:58:50 +0200

    Add book lists for all categories on StoreCategoriesPage

--- src/app/pages/store-categories-page/store-categories-page.component.ts
+++ src/app/pages/store-categories-page/store-categories-page.component.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core"
+import { DataService } from "src/app/services/data-service"
+import { RoutingService } from "src/app/services/routing-service"
 
 @Component({
 	selector: "pocketlib-store-categories-page",
 	templateUrl: "./store-categories-page.component.html",
 	styleUrls: ["./store-categories-page.component.scss"]
 })
-export class StoreCategoriesPageComponent {}
+export class StoreCategoriesPageComponent {
+	constructor(
+		public dataService: DataService,
+		private routingService: RoutingService
+	) {}
+
+	backButtonClick() {
+		this.routingService.NavigateBack("/store")
+	}
+}
