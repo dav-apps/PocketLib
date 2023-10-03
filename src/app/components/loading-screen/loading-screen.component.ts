b0ecabf5e6a6bfa92840bc88c8f358f965de4320
David <zurek.dav@gmail.com>
2023-10-03 21:43:47 +0200

    Add new logo for loading screen with black and white versions

--- src/app/components/loading-screen/loading-screen.component.ts
+++ src/app/components/loading-screen/loading-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core"
+import { DataService } from "src/app/services/data-service"
 
 @Component({
 	selector: "pocketlib-loading-screen",
@@ -7,4 +8,6 @@ import { Component, Input } from "@angular/core"
 })
 export class LoadingScreenComponent {
 	@Input() message: string = ""
+
+	constructor(public dataService: DataService) {}
 }
