f675882ffbdb5e1fbaa90c6abdf17c861fa20016
David <zurek.dav@gmail.com>
2024-11-23 00:03:18 +0100

    Install confetti package & show confetti on OrderConfirmationPage

--- src/app/pages/order-confirmation-page/order-confirmation-page.component.ts
+++ src/app/pages/order-confirmation-page/order-confirmation-page.component.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core"
+import { confetti } from "@tsparticles/confetti"
 
 @Component({
 	templateUrl: "./order-confirmation-page.component.html",
 	styleUrl: "./order-confirmation-page.component.scss"
 })
-export class OrderConfirmationPageComponent {}
+export class OrderConfirmationPageComponent {
+	ngOnInit() {
+		confetti({})
+	}
+}
